ee6bcb623deb3da5a9805936f9d64a8fdec0b7d0
laptop <og.kalashnikov@gmail.com>
2023-11-15 15:45:22 -0800

    linter fixes

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
-   
-@app.route('/')
 
+
+@app.route('/')
 def hello_world():
-    
+
     return 'Hello, World!'
 
-@app.route('/novaja_ficha')
 
+@app.route('/novaja_ficha')
 def bye():
     return 'sosi shljapu main12'
 
-@app.route('/username/<name>/<int:number>')
 
-def name(name,number):
-    return f"hello {name} , you are {number}!"
+@app.route('/username/<name>/<int:number>')
+def name(name, number):
+    return f"hello {name} ,you are {number}!"
+
 
 if __name__ == "__main__":
-    
+
     app.run(host="0.0.0.0", port=5000, debug=True)
-    
