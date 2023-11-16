e2fa88b92773347aff4a04aa7892efec3ce5af1d
laptop <og.kalashnikov@gmail.com>
2023-11-16 10:58:27 -0800

    commits

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route('/novaja_ficha')
 def bye():
-    return 'sosi shljapu branch linter test 123'
+    return 'sosi shljapu branch linter testing'
 
 
 @app.route('/username/<name>/<int:number>')
