d68ee63bc04d914ee026919f89fdc88d2be334fb
laptop <og.kalashnikov@gmail.com>
2023-11-22 11:56:26 -0800

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route('/novaja_ficha')
 def bye():
-    return 'sosi shljapu main linter check'
+    return 'sosi shljapu main linter check123'
 
 
 @app.route('/username/<name>/<int:number>')
