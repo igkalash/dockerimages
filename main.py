f776e37d7ebdd38f9bed1cf1766b93ea58399e1a
igkalash <og.kalashnikov@gmail.com>
2023-12-05 18:15:51 -0800

    tag test

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello_world():
 @app.route('/novaja_ficha')
 def bye():
 
-    return 'sosi shljapu main compose tag testing'
+    return 'sosi shljapu main compose tag testing123'
 
 
 @app.route('/username/<name>/<int:number>')
