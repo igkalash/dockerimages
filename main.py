6cab791555aa0d4be80f7a0362d15e36fa9cac03
laptop <og.kalashnikov@gmail.com>
2023-12-09 09:30:31 -0800

    tag test

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello_world():
 @app.route('/novaja_ficha')
 def bye():
 
-    return 'sosi shljapu main compose tag'
+    return 'sosi shljapu main compose tageqwe'
 
 
 @app.route('/username/<name>/<int:number>')
