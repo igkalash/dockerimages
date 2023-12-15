7b57f17513921aaa1392a2c23f31351f938c5c98
laptop <og.kalashnikov@gmail.com>
2023-12-15 14:22:46 -0800

    test

--- main.tf
+++ main.tf
@@ -14,8 +14,9 @@ provider "aws" {
 resource "aws_instance" "homework" {
   ami           = "ami-0cbd40f694b804622"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = ["sg-0400fda14a7993d65"]
+  key_name = "homework_key_pair"
   tags = {
-    Name = "homework-1"
+    Name = "homework tag"
   }
 }
