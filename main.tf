31c6f4aa55f918d1e39c246f05e204f6ce66607e
laptop <og.kalashnikov@gmail.com>
2024-01-12 09:17:51 -0800

    test

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "homework1" {
   ami           = "ami-08a7a30216d9c5784"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0400fda14a7993d65"]
+  security_groups = [aws_security_group.instances.name]
   key_name = "homework_key_pair"
   tags = {
     Name = "homework tag"
@@ -25,10 +25,146 @@ resource "aws_instance" "homework1" {
 resource "aws_instance" "homework2" {
   ami           = "ami-08a7a30216d9c5784"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0400fda14a7993d65"]
+  security_groups = [aws_security_group.instances.name]
   key_name = "homework_key_pair"
   tags = {
     Name = "homework tag"
   }
+}
+data "aws_vpc" "default_vpc" {
+  default = true
+}
 
+# data "aws_subnet_ids" "default_subnet" {
+#   vpc_id = data.aws_vpc.default_vpc.id
+# }
+
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "instances" {
+  name     = "example-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "homework1" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id        = aws_instance.homework1.id
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "homework2" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id        = aws_instance.homework2.id
+  port             = 8080
+}
+
+resource "aws_lb_listener_rule" "instances" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instances.arn
+  }
+}
+
+
+resource "aws_security_group" "alb" {
+  name = "alb-security-group"
+}
+
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+resource "aws_security_group_rule" "allow_alb_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+
+resource "aws_lb" "load_balancer" {
+  name               = "web-app-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default_subnet.ids
+  security_groups    = [aws_security_group.alb.id]
+
+}
+
+resource "aws_route53_zone" "primary" {
+  name = "devopsdeployed.com"
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "devopsdeployed.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.load_balancer.dns_name
+    zone_id                = aws_lb.load_balancer.zone_id
+    evaluate_target_health = true
+  }
 }
