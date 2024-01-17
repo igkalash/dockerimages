09b12b7b1c7ece751f5a445411e9fb754c20568c
laptop <og.kalashnikov@gmail.com>
2024-01-17 12:55:30 -0800

    t

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def hello_world():
 @app.route('/novaja_ficha')
 def bye():
 
-    return 'sosi shljapu main compose tqwe123'
+    return 'sosi shljapu main compose tqwe12'
 
 
 @app.route('/username/<name>/<int:number>')
